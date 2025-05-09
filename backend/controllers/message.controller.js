8a2ad2207ee9bc6e6890c0bb35da301a856cd215
amin <aminbouallegui0@gmail.com>
2025-05-10 00:41:18 +0200

    update postMessages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.config.js";
 
 
 export const GetUsers =async (req,res)=>{
@@ -54,5 +55,38 @@ export const GetMessages= async(req,res) =>{
     }catch(error){
         console.log("erreur au cours de exécution de la requete",error.message);
         res.status(500).json({message:"erreur au server"})
+    }
+}
+
+
+export const PostMessages= async(req,res)=>{
+    try{
+        const {text,Image} =req.body;
+        const {id:senderMessage} = req.params;
+        const recivedMessage = req.user._id;
+        let pic ;
+        if(Image){
+           const  UploadImg = await cloudinary.uploader.upload(Image);
+           pic = UploadImg.secure_url;
+        }
+
+        const NewMessages = new Message({
+            senderMessage,
+            recivedMessage,
+            text,
+            Image : pic,
+        })
+
+        if(!NewMessages){
+            res.status(404).json({message:'erreur au cours de transmi '})
+        }
+
+
+        res.status(201).json(NewMessages);
+
+    }catch(error){
+        console.log("erreur au cours de exécution de la requete",error.message);
+        res.status(500).json({message:"erreur au server"})
+
     }
 }
--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { protectRoute } from '../midleware/auth.midleware.js'
-import { GetUsers,GetUser } from '../controllers/message.controller.js';
+import { GetUsers,GetUser,GetMessages,PostMessages } from '../controllers/message.controller.js';
 
 const router=express.Router();
 
 router.get('/getUsers',protectRoute,GetUsers);
 router.get('/getUser',protectRoute,GetUser);
-
-router.get('/send/:id',protectRoute,GetMessages)
+router.post('/message/:id',protectRoute,PostMessages);
+router.get('/send/:id',protectRoute,GetMessages);
 
 
 export default router;
